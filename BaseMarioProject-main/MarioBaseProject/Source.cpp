400f4413051ed681bf820e9b08dca91afb880c5c
Edward Hill <115216135+EdwardJHill@users.noreply.github.com>
2023-03-10 09:40:02 +0000

    done finished up mate righty oh

--- BaseMarioProject-main/MarioBaseProject/Source.cpp
+++ BaseMarioProject-main/MarioBaseProject/Source.cpp
@@ -6,12 +6,16 @@
 #include <iostream>
 #include "Texture2D.h"
 #include "Commons.h"
+#include "GameScreenManager.h"
 
 using namespace std;
 //Globals
 SDL_Window* g_window = nullptr;
 SDL_Renderer* g_renderer = nullptr;
-Texture2D* g_texture = nullptr;
+
+GameScreenManager* game_screen_manager;
+
+Uint32 g_old_time;
 //Function prototypes
 bool InitSDL();
 void CloseSDL();
@@ -26,6 +30,9 @@ int main(int argc, char* args[])
 	
 	if (InitSDL())
 	{
+		game_screen_manager = new GameScreenManager(g_renderer, SCREEN_LEVEL1);
+		//set the time
+		g_old_time = SDL_GetTicks();
 		bool quit = false;
 		while (!quit)
 		{
@@ -62,7 +69,7 @@ bool InitSDL()
 				cout << "SDL_Image didn't work . my bad " << IMG_GetError();
 				return false;
 			}
-
+			/*
 			g_texture = new Texture2D(g_renderer);
 			if (!g_texture->LoadFromFile("Images/test.bmp"))
 			{
@@ -72,6 +79,7 @@ bool InitSDL()
 			{
 				return false;
 			}
+			*/
 			
 		}
 		else
@@ -102,11 +110,15 @@ void CloseSDL()
 	SDL_DestroyRenderer(g_renderer);
 	g_renderer = nullptr;
 	//release the texture
-	delete g_texture;
-	g_texture = nullptr;
+	//delete g_texture;
+	//g_texture = nullptr;
+	//destory the game screen manager
+	delete game_screen_manager;
+	game_screen_manager = nullptr;
 }
 bool Update()
-{
+{	
+	Uint32 new_time = SDL_GetTicks();
 	//Event handler
 	SDL_Event e;
 	//get events
@@ -117,6 +129,8 @@ bool Update()
 		return true;
 		break;
 	}
+	game_screen_manager->Update((float)(new_time - g_old_time) / 1000.0f, e);
+	g_old_time = new_time;
 	return false;
 }
 
@@ -126,7 +140,7 @@ void Render()
 	SDL_SetRenderDrawColor(g_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
 	SDL_RenderClear(g_renderer);
 
-	g_texture->Render(Vector2D(), SDL_FLIP_NONE);
+	game_screen_manager->Render();
 	//Render to Screen
 
 	//update the screen
